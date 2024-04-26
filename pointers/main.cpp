353f2c47db78235803b5510f2a7cfd77a76b4aba
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2024-04-26 22:15:06 +0300

    more solutions

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7};
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     std::cout << *(arr + 3) << std::endl;
 
     return 0;
